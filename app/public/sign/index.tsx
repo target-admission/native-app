8e61c1f581118c11c726473071ef7bc302eb29d2
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-28 23:38:21 +0600

    added: sign in form

--- app/public/sign/index.tsx
+++ app/public/sign/index.tsx
@@ -1,11 +1,29 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Input from "../../../src/components/Input";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
+import { Controller, useForm } from "react-hook-form";
+import { useLogin } from "../../../src/queries/auth";
+import handleResponse from "../../../src/utilites/handleResponse";
 
 export default function Page() {
 	const router = useRouter();
+	const {
+		register,
+		setValue,
+		handleSubmit,
+		control,
+		reset,
+		formState: { errors },
+	} = useForm();
+
+	const { mutateAsync, isLoading } = useLogin();
+
+	const onSubmit = async (data: any) => {
+		const res = await handleResponse(() => mutateAsync(data));
+		console.log(res);
+	};
+
 	return (
 		<SafeAreaView className="bg-black h-full flex flex-col">
 			<View className="flex-1 flex flex-col items-start justify-end p-8 gap-2">
@@ -24,12 +42,42 @@ export default function Page() {
 					</View>
 				</View>
 				<View className="w-full">
-					<Input placeholder="Phone Number" />
+					<Controller
+						control={control}
+						name="phone"
+						rules={{
+							required: true,
+						}}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								placeholder="Phone Number"
+								onChangeText={onChange}
+								keyboardType="phone-pad"
+								onBlur={onBlur}
+								value={value}
+								autoComplete="off"
+							/>
+						)}
+					/>
 				</View>
 				<View className="w-full">
-					<Input
-						secureTextEntry
-						placeholder="Password"
+					<Controller
+						control={control}
+						name="password"
+						rules={{
+							required: true,
+						}}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								placeholder="Password"
+								onChangeText={onChange}
+								secureTextEntry
+								selectTextOnFocus
+								autoComplete={"off"}
+								onBlur={onBlur}
+								value={value}
+							/>
+						)}
 					/>
 				</View>
 				<View className="flex flex-row items-center justify-end w-full pr-4">
@@ -40,10 +88,7 @@ export default function Page() {
 				<TouchableOpacity
 					className="relative rounded-md w-full"
 					activeOpacity={0.85}
-					onPressOut={async () => {
-						await AsyncStorage.removeItem("@app:openedBefore");
-						router.push("/onboarding");
-					}}
+					onPressOut={handleSubmit(onSubmit)}
 				>
 					<Text className="bg-primary rounded-md text-xl text-center font-fredoka-semibold tracking-widest  px-6 py-4">
 						SIGN IN
