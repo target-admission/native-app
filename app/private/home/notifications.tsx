3d514a5e580c60f6536933a14ddc580c7a564548
tam11a <ibrahimsadiktamim@gmail.com>
2023-10-13 01:26:32 +0600

    filter area of notification done

--- app/private/home/notifications.tsx
+++ app/private/home/notifications.tsx
@@ -1,8 +1,38 @@
+import React from "react";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const filters = [
+	{
+		id: "",
+		label: "All",
+	},
+	{
+		id: "news",
+		label: "News",
+	},
+	{
+		id: "reffer",
+		label: "Refferal",
+	},
+	{
+		id: "score",
+		label: "Score",
+	},
+	{
+		id: "payment",
+		label: "Payment",
+	},
+	{
+		id: "security",
+		label: "Security",
+	},
+];
+
 export default function Page() {
+	const [activeFilter, setActiveFilter] = React.useState(filters[0].id);
+
 	return (
 		<SafeAreaView className="bg-background h-full">
 			<View className="flex flex-row items-center justify-between px-5 py-4">
@@ -23,6 +53,7 @@ export default function Page() {
 				</View>
 				<TouchableOpacity
 					className="mr-3"
+					activeOpacity={0.6}
 					onPress={() => {
 						console.log("Mark all as read");
 					}}
@@ -34,6 +65,35 @@ export default function Page() {
 					/>
 				</TouchableOpacity>
 			</View>
+			<ScrollView
+				style={{ flex: 1 }}
+				horizontal
+				contentContainerStyle={{
+					flexDirection: "row",
+					columnGap: 10,
+					overflow: "scroll",
+					paddingLeft: 10,
+				}}
+			>
+				{filters?.map?.((item: { id: string; label: string }) => (
+					<TouchableOpacity
+						key={item.id}
+						onPress={() => setActiveFilter(item.id)}
+						activeOpacity={0.6}
+						disabled={activeFilter === item.id}
+					>
+						<Text
+							className={`text-base font-fredoka px-3 py-1 rounded-full ${
+								activeFilter === item.id
+									? "bg-primary text-background font-fredoka-semibold"
+									: "bg-gray-600 text-background font-fredoka-medium"
+							}`}
+						>
+							{item?.label}
+						</Text>
+					</TouchableOpacity>
+				))}
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
