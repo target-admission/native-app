cbb4549b1cf74c2c2f7bac585e3b9f58ff90bd51
tam11a <ibrahimsadiktamim@gmail.com>
2023-10-13 00:41:00 +0600

    notification page started

--- app/private/home/notifications.tsx
+++ app/private/home/notifications.tsx
@@ -1,11 +1,38 @@
-import { Text, View } from "react-native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
 	return (
 		<SafeAreaView className="bg-background h-full">
-			<View>
-				<Text className="text-white">Notifications</Text>
+			<View className="flex flex-row items-center justify-between px-5 py-4">
+				<View className="flex flex-row items-center gap-4">
+					<MaterialCommunityIcons
+						name="bell"
+						size={24}
+						color="white"
+					/>
+					<View className="flex flex-col items-start">
+						<Text className="text-white font-fredoka-medium text-xl">
+							Notifications
+						</Text>
+						<Text className="font-fredoka text-gray-500 text-base">
+							5 unread notifications
+						</Text>
+					</View>
+				</View>
+				<TouchableOpacity
+					className="mr-3"
+					onPress={() => {
+						console.log("Mark all as read");
+					}}
+				>
+					<Ionicons
+						name="checkmark-done"
+						size={24}
+						color="#ED2024"
+					/>
+				</TouchableOpacity>
 			</View>
 		</SafeAreaView>
 	);
